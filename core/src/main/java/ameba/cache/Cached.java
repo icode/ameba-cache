2335d1656ddd579045338e8a7e3ad14f7419953c
icode <intelligentcodemail@gmail.com>
2014-08-06 22:01:00 +0800

    部分基础实现

---
+++ core/src/main/java/ameba/cache/Cached.java
@@ -0,0 +1,17 @@
+package ameba.cache;
+
+import javax.ws.rs.NameBinding;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author icode
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+@NameBinding
+public @interface Cached {
+
+}

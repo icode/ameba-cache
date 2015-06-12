dcbad8472668888751360c5003ac8bf065de0e0a
icode <intelligentcodemail@gmail.com>
2015-06-13 03:13:06 +0800

    重构序列化接口和实现

--- core/src/main/java/ameba/cache/util/KryoSerializer.java
+++ core/src/main/java/ameba/cache/util/KryoSerializer.java
@@ -4,6 +4,7 @@ import ameba.util.IOUtils;
 import com.avaje.ebean.SqlRow;
 import com.avaje.ebean.bean.BeanCollection;
 import com.esotericsoftware.kryo.*;
+import com.esotericsoftware.kryo.factories.ReflectionSerializerFactory;
 import com.esotericsoftware.kryo.factories.SerializerFactory;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
@@ -135,18 +136,9 @@ public class KryoSerializer implements Serializer {
             register(LocalDateTime.class, new JodaLocalDateTimeSerializer());
             // guava ImmutableList
             ImmutableListSerializer.registerSerializers(this);
-            addDefaultSerializer(BeanCollection.class, new SerializerFactory() {
-                @Override
-                public com.esotericsoftware.kryo.Serializer makeSerializer(Kryo kryo, Class<?> type) {
-                    return new FieldSerializer<>(KryoExtends.this, BeanCollection.class);
-                }
-            });
-            addDefaultSerializer(SqlRow.class, new SerializerFactory() {
-                @Override
-                public com.esotericsoftware.kryo.Serializer makeSerializer(Kryo kryo, Class<?> type) {
-                    return new FieldSerializer<>(KryoExtends.this, SqlRow.class);
-                }
-            });
+            SerializerFactory fieldSerializerFactory = new ReflectionSerializerFactory(FieldSerializer.class);
+            addDefaultSerializer(BeanCollection.class, fieldSerializerFactory);
+            addDefaultSerializer(SqlRow.class, fieldSerializerFactory);
         }
 
         public KryoExtends() {
